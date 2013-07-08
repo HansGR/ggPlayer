65876020b0669fd0d814c7ac3b9e090efe8052ca
hans <hgr@mit.edu>
2013-07-08 13:09:13 -0400

    0.0 Initial commit:  add documentation (doxygen)

--- gg_arch/boardgame.py
+++ gg_arch/boardgame.py
@@ -14,9 +14,15 @@
 
 ## You should have received a copy of the GNU General Public License
 ## along with ggPlayer.  If not, see <http://www.gnu.org/licenses/>.
-
+"""@package boardgame
+Package containing the common classes for the gg architecture.  
+"""
 class Boardgame(object):
-    'Common boardgame class'
+    """
+    Common boardgame class
+	
+    This is an example of the basic class for a boardgame object.  Specific games (including boards, pieces, and rulesets) will be created as subclasses of this object.
+    """
     state = {} # Dictionary, cell name: current piece
     history = [] #list of moves
     pieces = []  #list of Gamepiece objects
@@ -25,24 +31,25 @@ class Boardgame(object):
     num_players = 0 #number of players
     
     def __init__(self, game):
+	"""Initializes the boardgame object"""
         self.game = game
 
     def getBoard(self):
-        'Return the current state of the board'
+        """Return the current state of the board"""
         return self.board
 
     def getHistory(self):
-        'Return the history of executed moves'
+        """Return the history of executed moves"""
         return self.history
     
     def make_move(self, piece_ID, cell_ID):
         # make_move goes through the following steps:
-		# 0. Verify piece_ID is legal to be moved this turn (belongs to Player)
-		# 1. Calculate all legal cells for piece_ID; confirm cell_ID is one of the legal cells
-		# 2. Check resulting state of gameboard to verify it is legal (e.g. not putting self into check)
-		# 3. Trigger actions associated with the move (kill attacked piece, promote pawn, switch to other player's turn, etc)
-		# 4. Return true when complete; return false, otherwise.  
-		return 0
+        # 0. Verify piece_ID is legal to be moved this turn (belongs to Player)
+        # 1. Calculate all legal cells for piece_ID; confirm cell_ID is one of the legal cells
+        # 2. Check resulting state of gameboard to verify it is legal (e.g. not putting self into check)
+        # 3. Trigger actions associated with the move (kill attacked piece, promote pawn, switch to other player's turn, etc)
+        # 4. Return true when complete; return false, otherwise.  
+        return 0
     
     def getCellIndex(self, cell_name):
         return self.state.find(cell_name)  # correct array function?
